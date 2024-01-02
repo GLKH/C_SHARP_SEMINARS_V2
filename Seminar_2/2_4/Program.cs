a605eab634545618dcf786735023d70d1d820b9c
Galina Kharitonova <kharitonovagl@gmail.com>
2024-01-03 00:17:00 +0300

    Seminar 2 task4

---
+++ Seminar_2/2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выводит третью с конца
+//цифру заданного числа или сообщает, что третьей цифры нет.
+// 456 => 6
+// 7812 => 8
+// 91 => Третьей цифры нет
+
+Console.Write("Enter a number:   ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n >= 100)
+{
+    Console.WriteLine(n % 1000 / 100);
+}
+else
+{
+    Console.WriteLine("There is no third digit");
+}
