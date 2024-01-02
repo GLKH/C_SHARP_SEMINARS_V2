a5d6c73063e512be67f97af4a94e63f6dbd4dcf1
Galina Kharitonova <kharitonovagl@gmail.com>
2024-01-03 00:36:38 +0300

    Seminar 2 task HW_1

---
+++ Seminar_2/HW_2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает
+//на вход число и проверяет, кратно ли оно одновременно 7 и 23
+//Пример
+// a=50 => нет
+// a=7 => нет
+// a=322 => да
+
+Console.Write("Enter a number:  ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
