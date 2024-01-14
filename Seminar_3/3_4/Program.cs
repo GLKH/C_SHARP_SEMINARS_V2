8c0661555be2d286f0327d510d273398763bbd9b
Galina Kharitonova <kharitonovagl@gmail.com>
2024-01-14 20:43:31 +0300

    Seminar_3:Task4

---
+++ Seminar_3/3_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Дано натуральное трёхзначное число. Создайте массив, состоящий из
+// цифр этого числа. Младший разряд числа должен располагаться на 0-
+// м индексе массива, старший – на 2-м.
+// Пример
+// 456 => [6 5 4]
+// 781 => [1 8 7]
+
+Console.Write("Enter a number between 100 and 999:   ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a >= 100 && a < 1000)
+{
+    Console.Write("[");
+    for (int i = 0; i < 3; i = i + 1)
+    {
+        Console.Write(Math.Truncate(a / Math.Pow(10, i) % 10) + " ");
+    }
+    Console.Write("]");
+}
+else
+{
+    Console.WriteLine("You entered a number outside the specified range. Please be careful!");
+}
