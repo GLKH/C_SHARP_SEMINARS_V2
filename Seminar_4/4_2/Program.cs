2dca82f2adeb3d62882636737285eeb4474585f1
Galina Kharitonova <kharitonovagl@gmail.com>
2024-01-28 19:57:12 +0300

    Seminar_4:Task2

---
+++ Seminar_4/4_2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте массив из N случайных целых чисел (N вводится с
+// клавиатуры).
+// Найдите количество чисел, которые оканчиваются на 1 и
+// делятся нацело на 7.
+// Пример
+// [1 5 11 21 81 4 0 91 2 3]    => 2
+
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    Random n_new = new Random();
+
+    for (int i = 0; i < size; i++)
+        arr[i] = n_new.Next(from, to);
+
+    return arr;
+}
+
+int CountNum(int[] arr)
+{
+    int Count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 7 == 0 && arr[i] % 10 == 1)
+        {
+            Count++;
+        }
+    }
+    return Count;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int result = CountNum(mass);
+Console.WriteLine(result);
+
