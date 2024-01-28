2b8dc857d19dc2ca47ad9f83a0c1981460c06bb9
Galina Kharitonova <kharitonovagl@gmail.com>
2024-01-28 20:19:33 +0300

    Seminar_4:Task3

---
+++ Seminar_4/4_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Заполните массив на N (вводится с консоли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из
+// массива. Старший разряд числа находится на 0-м индексе,
+// младший – на последнем.
+// Пример
+// [1 3 2 4 2 3] => 132423
+// [2 3 1] => 231
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    Random n_new = new Random();
+
+    for (int i = 0; i < size; i++)
+        arr[i] = n_new.Next(from, to);
+
+    return arr;
+}
+
+int NumberFromMass(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result = result*10 + arr[i];
+    }
+    return result;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+Print(mass);
+int res = NumberFromMass(mass);
+Console.WriteLine(res);
+
