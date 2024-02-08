3ed80404c9d6dd2c8194848c30c167613bae38ac
Galina Kharitonova <kharitonovagl@gmail.com>
2024-02-09 02:31:33 +0300

    Seminar_6:HW3

---
+++ Seminar_6/HW_6_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+// Пример
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string s, revs = "";
+        Console.WriteLine(" Enter string");
+        s = Console.ReadLine();
+        for (int i = s.Length - 1; i >= 0; i--) //String Reverse
+        {
+            Console.WriteLine(i);
+            revs += s[i].ToString();
+        }
+        if (revs == s) // Checking whether string is palindrome or not
+        {
+            Console.WriteLine("String is Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("String is not Palindrome");
+        }
+        Console.ReadKey();
+    }
+}
+
