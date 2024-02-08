41cf385bdfe680f90f4b1924bb9f216a13580642
Galina Kharitonova <kharitonovagl@gmail.com>
2024-02-08 23:34:47 +0300

    Seminar6:task3

---
+++ Seminar_6/6_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+string str = "Hello!";
+
+int CarrArr (string word)
+{
+    string vowels = "aeiouy";
+    int a = 0;
+    for (int i = 0; i < word.Length; i++)
+    {
+        if(vowels.Contains(word[i]))
+            a++;
+    }
+return a;
+}
+Console.WriteLine(CarrArr(str));
